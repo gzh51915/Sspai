1d5254157bec1ca5a29e081fb564e101a8d88a88
Mr.chelsea <3100805485@qq.com>
2020-06-13 14:18:11 +0800

    add react environment

---
+++ src/components/Frame/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import {Button} from 'antd'
+export default class Frame extends Component {
+    render() {
+        return (
+            <div>
+                <Button>了hi后</Button>
+            </div>
+        )
+    }
+}
