001ffbe3b243fc21344ec27e520c6f72bc7d2648
LavaCxx <a936269286@gmail.com>
2020-06-22 11:51:36 +0800

    add:build

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 var service = axios.create({
-  baseURL: "/api",
+  // baseURL:"/api", // 开发环境下使用
+  baseURL: "http://10.3.135.29:3000/",
 });
 
 export default service;
